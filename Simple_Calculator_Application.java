90a132dacdd7bbecd0c3a02f1961514392e32dd3
Jayesh Dambale <88830136+JAYESHDAMBALE@users.noreply.github.com>
2023-11-21 17:09:23 +0530

    Add files via upload
    Simple Calculator with java language. 
    Performing task like addition, subtraction, multiplication, division and Taking input from user.

---
+++ Simple_Calculator_Application.java
@@ -0,0 +1,57 @@
+package motion_cut;
+
+import java.util.Scanner;
+
+public class Simple_Calculator_Application {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Input two numbers
+        System.out.print("Enter the first number: ");
+        double num1 = scanner.nextDouble();
+
+        System.out.print("Enter the second number: ");
+        double num2 = scanner.nextDouble();
+
+        // Choose operation
+        System.out.println("Choose an operation:");
+        System.out.println("1. Addition");
+        System.out.println("2. Subtraction");
+        System.out.println("3. Multiplication");
+        System.out.println("4. Division");
+
+        int choice = scanner.nextInt();
+
+        double result = 0;
+
+        // Perform the selected operation
+        switch (choice) {
+            case 1:
+                result = num1 + num2;
+                break;
+            case 2:
+                result = num1 - num2;
+                break;
+            case 3:
+                result = num1 * num2;
+                break;
+            case 4:
+                if (num2 != 0) {
+                    result = num1 / num2;
+                } else {
+                    System.out.println("Error: Cannot divide by zero.");
+                    return;
+                }
+                break;
+            default:
+                System.out.println("Invalid choice");
+                return;
+        }
+
+        // Display the result
+        System.out.println("Result: " + result);
+
+        scanner.close();
+    }
+}
+
